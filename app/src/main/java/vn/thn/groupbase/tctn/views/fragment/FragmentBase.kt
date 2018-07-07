0b15aba87a497a87592b1cd1db54385d4c26102d
TruongHieuNghia <nghiath@mf-vn.com>
2018-07-07 10:14:27 +0700

    add notificationFragment

--- app/src/main/java/vn/thn/groupbase/tctn/views/fragment/FragmentBase.kt
+++ app/src/main/java/vn/thn/groupbase/tctn/views/fragment/FragmentBase.kt
@@ -1,5 +1,6 @@
 package vn.thn.groupbase.tctn.views.fragment
 
+import android.support.v4.app.FragmentTransaction
 import vn.thn.groupbase.lib.views.fragment.GBCommonFragment
 import vn.thn.groupbase.tctn.R
 
@@ -17,5 +18,12 @@ abstract class FragmentBase: GBCommonFragment() {
 
     override fun contentId(): Int {
         return 0;
+    }
+
+    override fun setAnimationCustom(animationCustom: FragmentTransaction) {
+        animationCustom.setCustomAnimations(R.anim.fragment_slide_right_enter,
+                R.anim.fragment_slide_left_exit,
+                R.anim.fragment_slide_left_enter,
+                R.anim.fragment_slide_right_exit)
     }
 }
---
+++ app/src/main/java/vn/thn/groupbase/tctn/views/fragment/NotificationFragment.kt
@@ -0,0 +1,24 @@
+package vn.thn.groupbase.tctn.views.fragment
+
+import vn.thn.groupbase.tctn.R
+
+/**
+ * Created by truonghieunghia on 7/6/18.
+ */
+class NotificationFragment: FragmentBase() {
+    override fun layoutContent(): Int {
+        return R.layout.fragment_notification
+    }
+
+    override fun firstInit() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun fragmentName(): String? {
+        return "NotificationFragment"
+    }
+}
